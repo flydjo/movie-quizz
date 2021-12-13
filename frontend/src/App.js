83cffc37c20eef1f1bbae309e073ea6f12f677bb
flydjo <lamantiajordan@gmail.com>
2021-12-13 14:55:37 +0100

    feature(Welcome-screen):Design v1 of the welcome screen with title, rules and start button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './css/App.css';
 import WelcomeScreen from "./pages/WelcomeScreen";
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <WelcomeScreen />
+      <Routes>
+        <Route exact path="/" element={<WelcomeScreen />} />
+      </Routes>
     </div>
   );
 }
--- frontend/src/pages/WelcomeScreen.js
+++ frontend/src/pages/WelcomeScreen.js
@@ -1,11 +1,31 @@
 import React from "react";
+import '../css/welcomeScreen.css';
+import {Link} from "react-router-dom";
 
 function WelcomeScreen() {
     return (
-        <div>
-            <h1>Welcome to the wonderful movie quizz !</h1>
+        <div className="welcome-screen-container">
+            {/* <h1 className="welcome-title">MOVIE QUIZZ</h1> */}
+            <svg className="welcome-title" viewBox="0 0 500 100">
+                <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
+                <text>
+                    <textPath startOffset="21%" alignment-baseline="middle" xlinkHref="#curve">
+                    MOVIE QUIZZ
+                    </textPath>
+                </text>
+            </svg>
+            <h1 className="welcome-sub-title">
+                Welcome to the movie quizz ! <br />
+                Let's test your knowledge about movie culture
+            </h1>
+            <p className="welcome-rules">
+                The rules are simple ! You'll be asked a series of "Yes or No" questions. Answer as many as you can in the allowed time ! Good luck !
+            </p>
+            <Link className="start-button" to="/">
+                START
+            </Link>      
         </div>
-    )
+    );
 }
 
 export default WelcomeScreen;
