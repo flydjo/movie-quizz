a992a0f62bcb035d5f1a2bda282299e1d3f1955f
flydjo <lamantiajordan@gmail.com>
2021-12-14 19:02:50 +0100

    feature(add-picture-quizz): change quizz time, 10 to 60 sec

--- frontend/src/hooks/useQuizzLogic.js
+++ frontend/src/hooks/useQuizzLogic.js
@@ -3,7 +3,7 @@ import {useState, useEffect} from "react";
 function useQuizzLogic() {
     //const apiKey = "f4360450df9f737a0ad0931c57e16402";
 
-    const GAME_TIME = 10;
+    const GAME_TIME = 60;
     const [timer, setTimer] = useState(GAME_TIME);
     const [isQuizzRunning, setIsQuizRunning] = useState(true);
 
